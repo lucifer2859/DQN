b0ae83045ffef8a218f7dae04eeb3905af42f29c
Ding Chen <107317258@qq.com>
2020-08-10 16:43:45 +0800

    Add files via upload

--- C51.py
+++ C51.py
@@ -85,11 +85,12 @@ class CategoricalDQN(nn.Module):
     def __init__(self, num_inputs, num_actions, num_atoms, Vmin, Vmax):
         super(CategoricalDQN, self).__init__()
         
-        self.num_inputs   = num_inputs
-        self.num_actions  = num_actions
-        self.num_atoms    = num_atoms
-        self.Vmin         = Vmin
-        self.Vmax         = Vmax
+        self.num_inputs  = num_inputs
+        self.num_actions = num_actions
+        self.num_atoms   = num_atoms
+        
+        self.Vmin = Vmin
+        self.Vmax = Vmax
         
         self.linear1 = nn.Linear(num_inputs, 128)
         self.linear2 = nn.Linear(128, 128)
@@ -102,11 +103,7 @@ class CategoricalDQN(nn.Module):
         x = F.relu(self.noisy1(x))
         x = self.noisy2(x)
         x = F.softmax(x.view(-1, self.num_atoms)).view(-1, self.num_actions, self.num_atoms)
-        return x # (batch_size, num_actions, num_atoms), (32, env.action_space.n, 51)
-        
-    def reset_noise(self):
-        self.noisy1.reset_noise()
-        self.noisy2.reset_noise()
+        return x
     
     def act(self, state):
         state = Variable(torch.FloatTensor(state).unsqueeze(0), volatile=True)
@@ -115,16 +112,20 @@ class CategoricalDQN(nn.Module):
         action = dist.sum(2).max(1)[1].numpy()[0]
         return action
 
+    def reset_noise(self):
+        self.noisy1.reset_noise()
+        self.noisy2.reset_noise()
+
 def projection_distribution(next_state, rewards, dones):
-    batch_size  = next_state.size(0)
+    batch_size = next_state.size(0)
     
     delta_z = float(Vmax - Vmin) / (num_atoms - 1)
     support = torch.linspace(Vmin, Vmax, num_atoms)
     
-    next_dist   = target_model(next_state).data.cpu() * support
+    next_dist = target_model(next_state).data.cpu() * support
     next_action = next_dist.sum(2).max(1)[1]
     next_action = next_action.unsqueeze(1).unsqueeze(1).expand(next_dist.size(0), 1, next_dist.size(2))
-    next_dist   = next_dist.gather(1, next_action).squeeze(1)
+    next_dist = next_dist.gather(1, next_action).squeeze(1)
         
     rewards = rewards.unsqueeze(1).expand_as(next_dist)
     dones   = dones.unsqueeze(1).expand_as(next_dist)
@@ -133,12 +134,13 @@ def projection_distribution(next_state, rewards, dones):
     Tz = rewards + (1 - dones) * 0.99 * support
     Tz = Tz.clamp(min=Vmin, max=Vmax)
     b  = (Tz - Vmin) / delta_z
-    l  = b.floor().long()
-    u  = b.ceil().long()
+    l  = b.floor().long() # Lower bound
+    u  = b.ceil().long()  # Upper bound
         
     offset = torch.linspace(0, (batch_size - 1) * num_atoms, batch_size).long()\
                     .unsqueeze(1).expand(batch_size, num_atoms)
 
+    # Equation (7)
     proj_dist = torch.zeros(next_dist.size())    
     proj_dist.view(-1).index_add_(0, (l + offset).view(-1), (next_dist * (u.float() - b)).view(-1))
     proj_dist.view(-1).index_add_(0, (u + offset).view(-1), (next_dist * (b - l.float())).view(-1))
@@ -181,7 +183,7 @@ def compute_td_loss(batch_size):
     
     dist = current_model(state)
     action = action.unsqueeze(1).unsqueeze(1).expand(batch_size, 1, num_atoms)
-    dist = dist.gather(1, action).squeeze(1) 
+    dist = dist.gather(1, action).squeeze(1)
     dist.data.clamp_(0.01, 0.99)
     loss = - (Variable(proj_dist) * dist.log()).sum(1).mean()
         
@@ -208,7 +210,7 @@ def plot(frame_idx, rewards, losses):
 ### Train ###
 num_frames = 10000
 batch_size = 32
-gamma      = 0.99
+gamma = 0.99
 
 losses = []
 all_rewards = []
@@ -243,9 +245,9 @@ for frame_idx in range(1, num_frames + 1):
 from common.wrappers import make_atari, wrap_deepmind, wrap_pytorch
 
 env_id = "PongNoFrameskip-v4"
-env    = make_atari(env_id)
-env    = wrap_deepmind(env)
-env    = wrap_pytorch(env)
+env = make_atari(env_id)
+env = wrap_deepmind(env)
+env = wrap_pytorch(env)
 
 num_atoms = 51
 Vmin = -10
@@ -262,11 +264,11 @@ optimizer = optim.Adam(current_model.parameters(), 0.0001)
 update_target(current_model, target_model)
 
 replay_initial = 10000
-replay_buffer  = ReplayBuffer(100000)
+replay_buffer = ReplayBuffer(100000)
 
 num_frames = 1000000
 batch_size = 32
-gamma      = 0.99
+gamma = 0.99
 
 losses = []
 all_rewards = []
